79bbe0bc119cf1cef0065eb89090e2b615654012
aleramosupa <a.ramos.2017@alumnos.urjc.es>
2018-03-16 16:16:23 +0100

    Innitial upload of newfile.py

---
+++ newflie.py
@@ -0,0 +1,29 @@
+a=1
+b=0
+
+print ("hello from Pycharm 1")
+print ("hello from Pycharm 2")
+
+print(a/b)
+
+
+print ("hello from Pycharm 3")
+print ("hello from Pycharm")
+print ("hello from Pycharm")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
